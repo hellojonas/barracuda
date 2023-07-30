bbed66d46d37d10f3378a43434d931230187d81b
Hello Jonas <hellojonashp@gmail.com>
2023-07-30 10:43:53 +0200

    load configuration from env

--- cmd/barracuda.go
+++ cmd/barracuda.go
@@ -1,19 +1,43 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	"log"
 	"net/http"
+	"os"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/hellojonas/barracuda/internal/barracuda"
+	_ "github.com/lib/pq"
 )
-
 
 func main() {
     r := chi.NewRouter()
     r.Use(middleware.Logger)
-    r.Mount("/api", barracuda.Routes())
+
+
+    datasource := os.Getenv("DATABASE_URL")
+
+    if datasource == "" {
+	log.Fatal("datasource not set")
+    }
+
+    db, err := sql.Open("postgres", datasource)
+
+    if err != nil {
+	log.Fatalf("error opening database connection. %v", err)
+    }
+
+    if err = db.Ping(); err != nil {
+	log.Fatalf("could not establish connection to database %v", err)
+    }
+
+    b := barracuda.New(db)
+    bRouter := barracuda.Router(b)
+
+    r.Mount("/api", bRouter)
 
     fmt.Println("Application listenning on port :8080")
     http.ListenAndServe(":8080", r)
--- go.mod
+++ go.mod
@@ -5,10 +5,10 @@ go 1.20
 require (
 	github.com/PuerkitoBio/goquery v1.8.1
 	github.com/go-chi/chi/v5 v5.0.10
+	github.com/lib/pq v1.10.9
 )
 
 require (
 	github.com/andybalholm/cascadia v1.3.1 // indirect
-	github.com/lib/pq v1.10.9 // indirect
-	golang.org/x/net v0.7.0 // indirect
+	golang.org/x/net v0.10.0 // indirect
 )
